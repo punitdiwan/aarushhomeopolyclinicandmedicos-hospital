2e52e7d7059e74566023cdb0b88001c989c10f90
shyam <shyam@maitretech.com>
2022-12-22 17:23:54 +0530

    update-gallery

--- src/component/Gallery.js
+++ src/component/Gallery.js
@@ -16,61 +16,46 @@ const Gallery = () => {
             <img src="./img/g1.jpg" alt="" />
           </div>
           <div className="gallery-col">
-            <img src="./img/g2.jpeg" alt="" />
+            <img src="./img/g2.jpg" alt="" />
           </div>
           <div className="gallery-col">
-            <img src="./img/g3.jpeg" alt="" />
+            <img src="./img/g3.jpg" alt="" />
           </div>
-
-          {/* <div className="gallery-col">
-            <img src="./img/g4.jpg" alt="" />
-          </div> */}
         </div>
-      </div>
-
-      {/* <div className="gallery">
         <div className="gallery-row">
+          <div className="gallery-col">
+            <img src="./img/g4.jpg" alt="" />
+          </div>
           <div className="gallery-col">
             <img src="./img/g5.jpg" alt="" />
           </div>
           <div className="gallery-col">
             <img src="./img/g6.jpg" alt="" />
           </div>
+        </div>
+        <div className="gallery-row">
           <div className="gallery-col">
             <img src="./img/g7.jpg" alt="" />
           </div>
-
           <div className="gallery-col">
             <img src="./img/g8.jpg" alt="" />
           </div>
-        </div>
-      </div> */}
-
-      <div className="gallery">
-        <div className="gallery-row">
-          <div className="gallery-col gallery-col-1">
+          <div className="gallery-col">
             <img src="./img/g9.jpg" alt="" />
           </div>
         </div>
+        {/* <div className="gallery-row">
+          <div className="gallery-col">
+            <img src="./img/g10.jpg" alt="" />
+          </div>
+          <div className="gallery-col">
+            <img src="./img/g11.jpg" alt="" />
+          </div>
+          <div className="gallery-col">
+            <img src="./img/g3.jpg" alt="" />
+          </div>
+        </div> */}
       </div>
-
-      {/* <div className="gallery">
-        <div className="gallery-row">
-          <div className="gallery-col">
-            <img src="./img/g1.jpg" alt="" />
-          </div>
-          <div className="gallery-col">
-            <img src="./img/g2.jpeg" alt="" />
-          </div>
-          <div className="gallery-col">
-            <img src="./img/g3.jpeg" alt="" />
-          </div>
-
-          <div className="gallery-col">
-            <img src="./img/g4.jpg" alt="" />
-          </div>
-        </div>
-      </div> */}
     </>
   );
 };
